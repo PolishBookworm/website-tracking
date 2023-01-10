2c3659f5cd1a5ad6c6d5ee42a6e3512796de5f92
oliwka <oliwka.pszona@gmail.com>
2023-01-10 18:45:32 +0100

    cleaned up code

--- main.py
+++ main.py
@@ -4,15 +4,26 @@ from requests import get
 from bs4 import BeautifulSoup
 from csv import reader, writer
 
+# Printing and updating list of checked websites
 print("These are the websites I am going to check:")
 with open("sites.csv") as file:
     data = reader(file)
     for row in data:
-        print(row)
-if input("Would you like to add more? (y/n) ") == "y":
+        print(f"{row[0]} ({row[1]})")
+while input("Would you like to add more? (y/n) ") == "y":
     with open("sites.csv", "a+") as file:
         write = writer(file)
-        while input("Continue? (y/n) ") == "y":
-            name = input("Enter site name: ")
-            url = input("Enter site url: ")
-            write.writerow([name, url])
+        name = input("Enter site name: ")
+        url = input("Enter site url: ")
+        tag = input("What tags would you like to check? ")
+        list_of_content = BeautifulSoup(
+            get(url).content, "html.parser").select(tag)
+        content = []
+        for element in list_of_content:
+            content.append(element.text)
+        write.writerow([name, url, tag, content])
+
+
+# TODO
+# * Finish, of course
+# * Denest
